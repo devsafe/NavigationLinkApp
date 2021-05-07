c0f80493956e120611340469e5a24d3c9a846fec
Rafinad <rafinad@outlook.com>
2021-05-07 19:21:30 +0300

    Initial Commit

---
+++ NavigationLinkApp/NavigationLinkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLinkAppApp.swift
+//  NavigationLinkApp
+//
+//  Created by Сахар Кубический on 07.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationLinkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
